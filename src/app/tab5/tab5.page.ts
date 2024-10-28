9e64c91445becd464b995f20c32e389b87013ec4
Kr-luis <kluis816@gmail.com>
2024-10-27 20:05:42 -0500

    prueba_actualizacion3

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Component({
   selector: 'app-tab3',
@@ -8,18 +9,22 @@ import { Component } from '@angular/core';
 export class Tab5Page {
   textToDownload: string = ''; // Variable para el texto
 
-  constructor() { }
+  constructor() {}
 
   // Función para descargar el texto como un archivo .txt
-  downloadTextFile() {
-    const blob = new Blob([this.textToDownload], { type: 'text/plain' });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'texto.txt';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
+  async downloadTextFile() {
+    try {
+      await Filesystem.writeFile({
+        path: 'texto.txt',
+        data: this.textToDownload,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      });
+
+      alert('Archivo guardado exitosamente en la carpeta de documentos');
+    } catch (e) {
+      console.error('Error guardando el archivo', e);
+      alert('No se pudo guardar el archivo.');
+    }
   }
 }
