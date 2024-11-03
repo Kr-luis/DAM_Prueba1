efa6b8580225680d2e861a76c61f97fba3c1e1fc
Kr-luis <kluis816@gmail.com>
2024-11-03 14:32:28 -0500

    Splash Screen

---
+++ src/app/splash-screen/splash-screen.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.scss']
+})
+export class SplashScreenComponent implements OnInit {
+  messages: string[] = [
+    'Bienvenido a nuestra aplicación!',
+    'Cargando recursos...',
+    'Preparando la experiencia...',
+    'Casi estamos listos...',
+    '¡Empezamos!'
+  ];
+  currentMessage: string = '';
+  messageIndex: number = 0;
+
+  ngOnInit() {
+    this.startSplashScreen();
+  }
+
+  startSplashScreen() {
+    this.updateMessage();
+
+    const intervalId = setInterval(() => {
+      this.messageIndex++;
+      if (this.messageIndex < this.messages.length) {
+        this.updateMessage();
+      } else {
+        clearInterval(intervalId);
+        // Aquí puedes navegar a la página principal o esconder el Splash Screen
+      }
+    }, 1000);
+  }
+
+  updateMessage() {
+    this.currentMessage = this.messages[this.messageIndex];
+  }
+}
